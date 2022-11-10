c795e3cc2f9f08de8182692ab51d2a9a14fd3c05
Maciej Kujawski <maciekujawski@gmail.com>
2022-11-10 20:28:32 +0100

    update z desktopa

---
+++ src/js/01-gallery.js
@@ -0,0 +1,28 @@
+import { galleryItems } from './gallery-items';
+// Add imports above this line
+// Change code below this line
+
+console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const newGallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <div><a class="gallery__item" 
+    href="${original}">
+    <img class="gallery__image" src="${preview}"
+    alt="${description}"
+    title="${description}" />
+    </a>
+    </div>
+    `;
+  })
+  .join('');
+
+const galleryPictures = document.querySelector('.gallery');
+
+galleryPictures.insertAdjacentHTML('beforeend', newGallery);
+//new SimpleLightbox('.gallery a', { /* options */ });
+const galleryLightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
